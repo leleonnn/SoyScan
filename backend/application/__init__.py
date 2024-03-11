26d4809ed152e39d901a859576f011255a60c9d1
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2024-03-11 12:51:12 +0700

    feat: create connection into mongodb atlas

---
+++ backend/application/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path="../.env")
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["MONGO_URI"] = os.getenv("MONGO_URI")
+
+
+try:
+    # Setup MongoDB
+    mongodb_client = PyMongo(app)
+    db = mongodb_client.db
+except Exception as e:
+    print("Error connecting to MongoDB:", e)
+
+from application import routes
