105ac080c479b409185f8bb655cdd895ed8977a7
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2024-03-25 04:08:45 +0700

    fix: fix bug at add_disease

--- backend/application/routes.py
+++ backend/application/routes.py
@@ -13,7 +13,7 @@ def index():
 def add_disease():
     try:
 
-        body = request.json()
+        body = request.json
         name = body["name"]
         rarity = body["rarity"]
         symptoms = body["symptoms"]
@@ -22,7 +22,7 @@ def add_disease():
         imageLink = body["imageLink"]
 
         db.disease.insert_one({
-            "_id": ObjectId(),
+            # "_id": ObjectId(),
             "name": name,
             "rarity": rarity,
             "symptoms": symptoms,
@@ -31,6 +31,17 @@ def add_disease():
             "imageLink": imageLink
         })
         return jsonify({"message": "Disease added successfully"})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+    
+
+@app.route("/api/disease", methods=["GET"])
+def get_disease_by_name():
+    try:
+        body = request.json
+        name = body["name"]
+        diseases = db.disease.find_one({'name': name})
+        return dumps(diseases)
     except Exception as e:
         return jsonify({"error": str(e)})
     
