1696c0bdee4b1a8f7ac89bf1a3790daff11b4d1a
leleonnn <leonardmars63@gmail.com>
2024-03-31 16:56:14 +0700

    feat: add custom defined colors

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -13,6 +13,14 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'green-1': '#B0DAAD',
+      'green-2': '#60815D',
+      'green-3': '#506B4D',
+      'green-3': '#455A43',
+      'yellow-1': '#F5E0B9',
+      'white-1': '#F4FFFA'
+    },
   },
   plugins: [],
 };
