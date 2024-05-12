4e75d486d39d4e6a8565db48cd8ebfe8c94e5bc4
leleonnn <leonardmars63@gmail.com>
2024-05-13 06:20:47 +0700

    feat: add Camera Page

--- frontend/src/app/Camera/page.jsx
+++ frontend/src/app/Camera/page.jsx
@@ -3,8 +3,27 @@ import Navbar from ".././components/Navbar";
 
 export default function Camera() {
   return (
-    <main>
+    <main className="flex flex-col justify-between w-full">
       <Navbar camera="true"/>
+      <div className="flex relative h-screen w-full bg-white-1 items-center justify-center">
+        <div className="flex flex-col items-center">
+          <p className="text-green-4 text-6xl font-bold ">
+          Take a Picture
+          </p>
+          <p className="text-green-4 text-3xl font-normal mt-3">
+          Take a picture of soybean leaf image by pressing button below
+          </p>
+          <div className="flex flex-row mt-7 items-center">
+            <div className="w-96 h-96 rounded-2xl bg-green-4 mr-7 border-4 border-green-1">
+
+            </div>
+            <button className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-3xl w-16 h-16 mt-7 transition-transform duration-300 transform hover:scale-110 active:scale-95">
+
+            </button>
+          </div>
+
+        </div>
+      </div>
     </main>
   );
 }
