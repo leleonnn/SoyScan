2140022aca1c9dff47c6b6df9d1ddf81fe991aa1
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2024-05-25 17:47:00 +0700

    feat: add uploading code, add image preview after uploading

--- frontend/src/app/Upload/page.jsx
+++ frontend/src/app/Upload/page.jsx
@@ -1,17 +1,15 @@
 "use client";
 
 import Image from "next/image";
-import Navbar from ".././components/Navbar";
+import Navbar from "../components/Navbar";
 import upload from "../../../public/upload.png";
-export { POST } from "next-s3-upload/route";
-import { useS3Upload } from "next-s3-upload";
-import { Fragment, useRef, useState } from 'react';
+import { useState } from "react";
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from "uuid";
 
 export default function Upload() {
-
   const [file, setFile] = useState(null);
+  const [uploadedFileName, setUploadedFileName] = useState("");
 
   const Bucket = process.env.NEXT_PUBLIC_S3_UPLOAD_BUCKET;
   const s3 = new S3Client({
@@ -21,8 +19,6 @@ export default function Upload() {
       secretAccessKey: process.env.NEXT_PUBLIC_S3_UPLOAD_SECRET,
     },
   });
-
-
 
   const handleUploadLocalFile = (e) => {
     e.preventDefault();
@@ -44,47 +40,60 @@ export default function Upload() {
         Body: file,
       });
       await s3.send(uploadToS3);
-      console.log("File uploaded successfully.");
+      setUploadedFileName(file.name);
+      console.log("File uploaded successfully:", fileName);
     } catch (error) {
       console.error("Error uploading file:", error);
     }
   };
-  
+
   return (
     <main className="flex flex-col justify-between w-full">
-      <Navbar upload="true"/>
+      <Navbar upload="true" />
       <div className="flex relative h-screen w-full bg-white-1 items-center justify-center">
         <div className="flex flex-col items-center">
-          <p className="text-green-4 text-6xl font-bold ">
-          Upload Image
-          </p>
+          <p className="text-green-4 text-6xl font-bold">Upload Image</p>
           <p className="text-green-4 text-3xl font-normal mt-3">
-          Upload a soybean leaf image by pressing button below
+            Upload a soybean leaf image by pressing the button below
           </p>
-          <div>
-            {/* <input type="file" onChange={handleFileChange} />
-            <button className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-xl w-56 h-16 mt-7 justify-center items-center hover:bg-purple-200 transition-transform duration-300 transform hover:scale-110 active:scale-95">
-              <Image 
-              alt=""
-              className="mr-3"
-              src={upload} />
-              Upload Image
-            </button> */}
+          {!file ? (
             <div className="flex items-center justify-center mb-4">
-              <label htmlFor="file-upload" className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-xl w-56 h-16 mt-7 justify-center items-center hover:bg-purple-200 transition-transform duration-300 transform hover:scale-110 active:scale-95">              
+              <label
+                htmlFor="file-upload"
+                className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-xl w-56 h-16 mt-7 justify-center items-center hover:bg-purple-200 transition-transform duration-300 transform hover:scale-110 active:scale-95"
+              >
                 <Image alt="" className="mr-3" src={upload} /> Upload Image
               </label>
-              <input id="file-upload" type="file" className="hidden" onChange={handleUploadLocalFile}/>
+              <input
+                id="file-upload"
+                type="file"
+                className="hidden"
+                onChange={handleUploadLocalFile}
+              />
             </div>
-            <div>
-              <button onClick={handleUploadS3}>S3 Upload</button>
+          ) : (
+            <div className="flex flex-col items-center">
+              <img
+                src={URL.createObjectURL(file)}
+                alt="Preview"
+                className="mb-4 max-h-96"
+              />
+              {file.name && (
+                <div className="mt-4">
+                  <p>Uploaded File Name: {file.name}</p>
+                </div>
+              )}
+              <button
+                onClick={handleUploadS3}
+                className="flex bg-green-1 text-white font-bold py-2 px-4 rounded-xl mt-4"
+              >
+                S3 Upload
+              </button>
             </div>
-          </div>
+          )}
 
         </div>
-
       </div>
-
     </main>
   );
 }
