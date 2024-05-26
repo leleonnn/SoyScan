893b1450a672f84ad5a88f8e3a9978a52127247a
leleonnn <leonardmars63@gmail.com>
2024-05-26 08:07:02 +0700

    fix: upload page error

--- frontend/src/app/Upload/page.jsx
+++ frontend/src/app/Upload/page.jsx
@@ -1,59 +1,8 @@
-"use client";
-
-import React, { useState} from "react";
-
 import Image from "next/image";
 import Navbar from ".././components/Navbar";
 import upload from "../../../public/upload.png";
 
 export default function Upload() {
-  const [selectedImage, setSelectedImage] = useState(null);
-
-  const handleImageChange = async (e) => {
-    // getBase64(e.target, (e) => {
-    //   setEventProfileUrl(e);
-    // });
-    // console.log(thisEventProfileUrl);
-    // setHasEventProfileUrl(true);
-    
-    const file = e.target.files[0];
-    console.log(file);
-    setSelectedImage(file);
-
-    const formData = new FormData();
-    formData.append('image', selectedImage);
-    console.log(formData);
-
-    try {
-      const response = await fetch(`https://localhost:3000/x`, {
-        method: 'POST',
-        body: formData,
-      });
-
-      if (response.ok) {
-        console.log('Image uploaded successfully');
-      } else {
-        console.error('Failed to upload image');
-      }
-    } catch (error) {
-      // setImageError('true');
-      console.error('Error uploading image:', error);
-    }
-  };
-
-  // const getBase64 = (fileInput, callback) => {
-  //   if (fileInput.files.length > 0) {
-  //     const reader = new FileReader();
-  //     reader.onload = (event) => {
-  //       const base64Image = event.target.result;
-  //       callback(base64Image);
-  //       setThisEventProfileUrl(base64Image);
-  //     };
-  //     reader.readAsDataURL(fileInput.files[0]);
-  //   }
-  // };
-
-
   return (
     <main className="flex flex-col justify-between w-full">
       <Navbar upload="true"/>
@@ -65,26 +14,13 @@ export default function Upload() {
           <p className="text-green-4 text-3xl font-normal mt-3">
           Upload a soybean leaf image by pressing button below
           </p>
-          <label
-              className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-xl w-56 h-16 mt-7 justify-center items-center hover:bg-purple-200 transition-transform duration-300 transform hover:scale-110 active:scale-95 hover:cursor-pointer"
-              htmlFor="files"
-          >
-            <input
-              type="file"
-              className="mt-6 items-center justify-center rounded-xl border-none absolute hidden "
-              id="files"
-              accept="image/png, image/gif, image/jpeg"
-              onChange={handleImageChange}
-            />
+          <button className="flex bg-white-1 text-green-1 font-bold border-4 border-green-1 shadow-xl shadow-green-1/30 py-2 px-4 rounded-xl w-56 h-16 mt-7 justify-center items-center hover:bg-purple-200 transition-transform duration-300 transform hover:scale-110 active:scale-95">
             <Image 
               alt=""
               className="mr-3"
-              src={upload} 
-            />
-            <p>
+            src={upload} />
               Upload Image
-            </p>
-          </label>
+          </button>
         </div>
       </div>
 
