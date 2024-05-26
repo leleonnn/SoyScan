71fc14c082fb1938703167d872e35d2cd88fa935
RayhanAdii <rayhan.a.wicaksono@gmail.com>
2024-05-27 05:27:07 +0700

    feat: move landing page from FrontendRazza

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,31 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <Navbar/>
-    </main>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white-1">
+      <Navbar />
+      <main className="flex flex-1 flex-col items-center justify-center">
+        <h1 className="text-6xl font-bold text-green-4">SoyScan</h1>
+        <p className="text-2xl text-green-3 mt-2">
+          Soybean Disease Identification at Your Fingertips
+        </p>
+        <div className="mt-10 flex space-x-4">
+          <Link
+            href="/Camera"
+            className="flex items-center justify-center bg-green-1 text-green-4 font-bold py-4 px-6 rounded-lg shadow-lg transition-transform transform hover:scale-105"
+          >
+            Take a Picture
+          </Link>
+          <Link
+            href="/Upload"
+            className="flex items-center justify-center bg-green-1 text-green-4 font-bold py-4 px-6 rounded-lg shadow-lg transition-transform transform hover:scale-105"
+          >
+            Upload Image
+          </Link>
+        </div>
+      </main>
+    </div>
   );
 }
